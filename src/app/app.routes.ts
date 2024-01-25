e37793de959fcb90f0f0fd1342590835c22f03b4
Pranav Jayachandran <pranavj@GMG.LOCAL>
2024-01-25 17:47:42 +0530

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { HomeComponentComponent } from './home-component/home-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "home", component: HomeComponentComponent
+    },
+    {
+        path: "todo-list", loadChildren: () => {
+            return loadRemoteModule({
+                remoteEntry: "http://localhost:4201/remoteEntry.js",
+                remoteName: "mfeApp",
+                exposedModule: "./TodoListModule"
+            }).then((m) => m.TodoListModule).catch((err) => {
+                console.log(err);
+            })
+        }
+    }
+];
